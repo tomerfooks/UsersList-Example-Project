3c9bb0b54aede24f992882c9796fcfd661ebe44b
Tomer Fooks <37155624+tomerfooks@users.noreply.github.com>
2021-04-04 23:00:49 +0300

    Jest Unit Tests (Backend)
    Added 2 Basic Unit Tests for Login & SignUp.

---
+++ back/tests/testSignupUser.test.js
@@ -0,0 +1,30 @@
+//HTTPS FOR MAKING HTTP REQUESTS FROM EXPRESS
+const https = require("https")
+describe("User Login Test", () => {
+  //THESE ARE THE ADMINISTRATOR
+  const email = "tomer@fooks.co.il"
+  const pass = "Tomer123!"
+
+  it("Testing User Login..", () => {
+    const options = {
+      hostname: 'http://localhost:4000/users/create"',
+      path: "/users/create",
+      port: 4000,
+      method: "POST",
+      body: JSON.stringify({
+        email,
+        pass,
+        ip: "100.100.100.10",
+      }),
+      headers: {
+        "Content-Type": "application/json",
+      },
+    }
+
+    const req = https.request(options, (res) => {
+      res.on("data", (d) => {
+        expect(data.body).toHaveProperty("token")
+      })
+    })
+  })
+})
